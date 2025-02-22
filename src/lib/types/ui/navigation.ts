ca75f15374cd03d12493eef62b10b373a5c2c678
tal-rofe <talrofe111+1@gmail.com>
2025-02-22 12:37:44 +0200

    refactor: 💡 dummy change

--- src/lib/types/ui/navigation.ts
+++ src/lib/types/ui/navigation.ts
@@ -2,7 +2,7 @@ import icons from '@/assets/icons';
 import { RoutesPath } from './route';
 
 export type NavElement = {
-	readonly label: string;
 	readonly path: RoutesPath;
+	readonly label: string;
 	readonly icon?: keyof typeof icons;
 };
